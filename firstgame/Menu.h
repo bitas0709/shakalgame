41ec3f0f9692d20b12f3d20e4d01a5985420be94
Станислав <stasolet@gmail.com>
2018-03-04 17:17:12 +0300

    Revert "Revert "Revert "Подраскидал функции в классы для более удобной работы с ними. Большая часть уже реализованного функционала так же не работает"""
    This reverts commit bbe8eb365d04d8a5178afaac74b65da6c18d50ff.

--- firstgame/Menu.h
+++ firstgame/Menu.h
@@ -1,16 +1,15 @@
 #pragma once
-#include "Global.h"
-class Menu : public Global
+class Menu
 {
 public:
 	bool mtrue = false; //переменная, отвечающая за запуск игры из меню. true - запуск
 	int mx = 35, my = 10; //координаты курсора в меню по умолчанию
+	int mwx = 80, mwy = 25; //размеры окна по умолчанию (main window x, main window y)
 	static const short str = 4; //количество строк в меню
 	static const short chr = 10; //количество символов в строке кнопки
 	short wx = ((mwx - chr - 4) / 2), wy = ((mwy - str - 2) / 2);
 	char butt[str][chr]; //массив, содержащий текст кнопок
 	void wind(); //функция, отвечающая за отрисовку окон
-	void key(); //функция, отвечающая за отрисовку кнопок в окне
 	void about(); //функция, отвечающая за вывод окна с информацией о разработчиках
 	Menu();
 	~Menu();
